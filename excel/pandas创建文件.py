f6d024e0ca2c3817b281f7ac716f086deb4d1971
zhaojuncai <330968157@qq.com>
2024-04-17 09:52:35 +0800

    删除没用文件

--- excel/pandas创建文件.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-os.chdir(r'D:\study\test')
-datt = pd.DataFrame({'序号': [1, 2, 3], '姓名': ['孙悟空', '孙行者', '行者孙']})
-datt = datt.set_index('序号')
-datt.to_excel('zjc666.xlsx')
--- excel/搜索匹配文件名fnmatch.py
+++
@@ -1,4 +0,0 @@
-import os
-import pandas as pd
-patth = os.chdir(r'D:\study\test')
-gzb = pd.read_excel('A')
